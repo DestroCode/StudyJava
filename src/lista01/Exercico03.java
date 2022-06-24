5ff850674ab64ae3df106544b9e54eb29ba099c8
DestroCode <aristonlanps@gmail.com>
2022-06-24 15:53:52 -0300

    Terceiro  exercico da lista!

---
+++ src/lista01/Exercico03.java
@@ -0,0 +1,23 @@
+package lista01;
+//Criar um programa que leia o peso e a altura 
+//do usuário e imprima no console o IMC.
+
+import java.util.Scanner;
+
+public class Exercico03 {
+public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+	System.out.println("Digite os valores segundo ");
+    //entrada de valores
+	System.out.print("peso:. ");
+    double peso = entrada.nextDouble();
+    //entrada peso
+    System.out.print("altura:. ");
+    double altura = entrada.nextDouble();
+    //entrada altura
+    altura *= altura;
+    double IMC = peso/altura;
+    System.out.printf("O seu IMC e %.2f", IMC);
+    
+}
+}
