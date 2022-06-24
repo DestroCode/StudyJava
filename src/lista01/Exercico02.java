9a08dad249aae0bf78082464116719989e8607d1
DestroCode <aristonlanps@gmail.com>
2022-06-24 15:35:31 -0300

    Segundo exercico da lista!

---
+++ src/lista01/Exercico02.java
@@ -0,0 +1,20 @@
+package lista01;
+//Criar um programa que leia a temperatura em
+//Celsius e converta para Fahrenheit.
+import java.util.Scanner;
+
+public class Exercico02 {
+public static void main(String[] args) {
+	try (Scanner entrada = new Scanner(System.in)) {
+		System.out.println("Digite o valor a ser convertido ");
+		//entrada de valores
+		System.out.print("C :. ");
+		double C = entrada.nextDouble();
+		//entrada F
+		double F = (C * 1.8) + 32;
+		//System.out.println(F);
+		//System.out.println(C);
+		System.out.printf("%.2f C em Fahrenheit %.2f F", C,F);
+	}
+}
+}
