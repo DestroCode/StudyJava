77c5249553d4e7662b242071415a46d5ba4c0176
DestroCode <aristonlanps@gmail.com>
2022-07-01 17:16:28 -0300

    Implementando estrutura de repeticao for

---
+++ src/controle/For1.java
@@ -0,0 +1,21 @@
+package controle;
+
+public class For1 {
+	public static void main(String[] args) {
+//		int contador = 1;
+//		while (contador <= 10) { 
+//			System.out.println("Bom dia");
+//			contador ++;
+//		}
+		// for (definicao da variavel; expressao; incremento)        
+		for (int contador = 0; contador <= 20; contador ++ ) {
+			System.out.printf("\nBom dia %s",contador);
+		}
+		
+		//laco infinito
+//		for(;;) {
+//			System.out.println("Fim!!");
+//		}
+	}
+
+}
