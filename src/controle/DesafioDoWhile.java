21b92382cb7d74c77f57861b402f61ff692a4bf7
DestroCode <aristonlanps@gmail.com>
2022-07-07 21:49:30 -0300

    Correcao do desafio DoWhile

--- src/controle/DesafioDoWhile.java
+++ src/controle/DesafioDoWhile.java
@@ -11,24 +11,24 @@ public class DesafioDoWhile {
 		int contador=0;
 		double validador;
 		System.out.println("Faremos a media / Para sair -1");
-		
+		//abertura da entrada de dados
 		do {
-			System.err.print("Digite sua nota:. ");
-			validador = entrada.nextDouble();
-			if (validador <= 10 && validador >= 0) {
+			System.err.print("Informe a nota:. ");
+			validador = entrada.nextDouble(); // recebendo a variavel para ser validada
+			if (validador <= 10 && validador >= 0) { //validou atribui ++
 				nota += validador;
 				contador++;
-			} else if(validador!=-1){
+			} else if(validador!=-1){ // caso seja diferente da saida 
 				System.out.println("Valor incorreto! Respeite o intervalo de 0 a 10");
 			}
 		} while (validador != -1);
-		double media = nota/contador;
+		double media = nota/contador; // calcula a media
 		
 		System.out.printf("A media da turma e %.2f", media);
 		System.out.println("\nEspero ter ajudado!! " + nota + " / " + contador);
 		
-
 		entrada.close();
+        //fim!
 	}
 
 }
