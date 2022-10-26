3abee47468558ec7f792609f3ba1cb9b25ecc200
DestroCode <aristonlanps@gmail.com>
2022-10-25 22:55:29 -0300

    Controle Break

---
+++ src/controle/Break.java
@@ -0,0 +1,18 @@
+package controle;
+
+import java.util.Iterator;
+
+public class Break {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if(i == 5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Fim!");
+	}
+
+}
