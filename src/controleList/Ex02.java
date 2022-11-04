c3054674a84f247f90b24024e009bfd528ceb4e4
DestroCode <aristonlanps@gmail.com>
2022-11-03 23:51:21 -0300

    Try exerc 2

---
+++ src/controleList/Ex02.java
@@ -0,0 +1,24 @@
+package controleList;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		double valor;
+		System.out.println("Digite um ano:");
+	   
+		valor = entrada.nextInt();
+		//double res = valor/100;
+		if(valor%400 == 0) {
+			System.out.printf("%.0f e bissexto!",valor);
+		}else if(valor%4 == 0) {
+			System.out.printf("%.0f e bissexto!",valor);
+		}else {
+			System.out.printf("%.0f nao bissexto!",valor);
+		}
+		entrada.close();
+	}
+
+}
