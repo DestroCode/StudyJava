f54ef8d78907954ddcc9de858d8e8c7f79e95171
DestroCode <aristonlanps@gmail.com>
2023-02-21 21:13:32 -0200

    First Class test

---
+++ src/controleList/Ex06.java
@@ -0,0 +1,21 @@
+package controleList;
+
+public class Ex06 {
+    public static void main(String [] args)
+
+   {
+
+          System.out.printf("Nome: Joao Paulo Silva\n");
+
+          System.out.printf("Idade: 23 anos\n");
+
+          System.out.printf("\nDias em que trabalhou em Janeiro:\n");
+
+          System.out.printf("1\t2\t5\t7\t13\t22\t27");
+          int x = 6%2;
+          
+          System.out.println("" + x);
+
+    }
+
+}
