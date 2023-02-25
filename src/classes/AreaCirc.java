bc7217f531296d231cf8a63faa62150cee9a9818
DestroCode <aristonlanps@gmail.com>
2023-02-24 23:10:36 -0200

    Change AreaCirc and AreaCircTeste

--- src/classes/AreaCirc.java
+++ src/classes/AreaCirc.java
@@ -4,10 +4,17 @@ public class AreaCirc {
 	double raio;
 	static double pi=3.14;
 	
+	//Creating constants with FINAL, never change
+	//final static double PI=3.1415;
+	
 	AreaCirc(double raio){
 		this.raio = raio;
 	}
 	double area() {
+		return pi * Math.pow(raio, 2);
+	}
+	//static method
+	static double area(double raio) {
 		return pi * Math.pow(raio, 2);
 	}
 
--- src/classes/AreaCircTeste.java
+++ src/classes/AreaCircTeste.java
@@ -4,7 +4,13 @@ public class AreaCircTeste {
 	public static void main(String[] args) {
 		
 		AreaCirc a1 = new AreaCirc(5.6);
+		AreaCirc a2 = new AreaCirc(6);
+		//change static attribute
+		AreaCirc.pi=3.1415;
+		
+		System.out.println(a2.area());
 		System.out.println(a1.area());
+		System.out.println(AreaCirc.area(15.0));
 		
 	}
 
