e39f4da025b70a8e9b9cb00027017875024d72ed
DestroCode <aristonlanps@gmail.com>
2023-02-26 19:58:52 -0300

    First array challenge

---
+++ src/array/Aluno.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class Aluno {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite quantas notas serao informadas :");
+	    int valor = entrada.nextInt();
+		
+	    double[] notas = new double[valor];
+	    System.out.println("Informe as notas");
+	    for (int i = 0; i < notas.length; i++) {
+			System.out.print("n" + (i+1) + ": ");
+			notas[i]=entrada.nextDouble();
+		}
+	    double total = 0.0;
+	    for(double nota:notas) {
+	    	total += nota;
+	    }
+	    
+	    System.out.println("Sua media e " + total/notas.length);
+		
+		entrada.close();
+	}
+
+}
