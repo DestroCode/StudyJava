f34fe431c0a313efac669279b0d670456be177c3
DestroCode <aristonlanps@gmail.com>
2023-02-27 21:33:14 -0300

    New Equals

--- src/classes/Equals.java
+++ src/classes/Equals.java
@@ -1,6 +1,6 @@
 package classes;
 
-import java.util.Date;
+//import java.util.Date;
 
 public class Equals {
 	
@@ -18,7 +18,7 @@ public class Equals {
 		System.out.println(u1.equals(u2));
 		System.out.println(u2.equals(u1));
 		
-		System.out.println(u1.equals(new Date()));
+		//System.out.println(u1.equals(new Date()));
 		
 	}
 	
