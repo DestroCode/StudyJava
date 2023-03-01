964c629ae0a2f35a17d649279d5bf4f6eaaa0f39
DestroCode <aristonlanps@gmail.com>
2023-02-28 23:41:10 -0300

    Application of list collections

---
+++ src/colecoes/Lista.java
@@ -0,0 +1,32 @@
+package colecoes;
+
+import java.util.ArrayList;
+
+public class Lista {
+	public static void main(String[] args) {
+		ArrayList<Usuario> lista = new ArrayList<>();
+		Usuario u1 = new Usuario("Anna");
+		lista.add(u1);
+		lista.add(new Usuario("Carlos"));
+		lista.add(new Usuario("Lia"));
+		lista.add(new Usuario("Bia"));
+		lista.add(new Usuario("Manu"));
+		
+		System.out.println(lista.get(3)); //acess index
+		
+		//lista.remove(1); //remove
+		System.out.println(">>>>" + lista.remove(1));
+		System.out.println(lista.remove(new Usuario("Manu")));
+		
+		System.out.println("Tem?" + lista.contains(new Usuario("Lia")));
+		System.out.println("Tem?" + lista.contains(u1));
+		
+		
+		
+		for(Usuario u: lista) {
+			System.out.println(u.nome);
+		}
+		
+	}
+
+}
