4bdb55b850fb6fccf3c474e1c4836620b54948e0
DestroCode <aristonlanps@gmail.com>
2023-03-04 12:32:58 -0300

    Finish Class Stack

--- src/colecoes/Pilha.java
+++ src/colecoes/Pilha.java
@@ -1,28 +1,33 @@
 package colecoes;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Pilha {
 	
 	public static void main(String[] args) {
 		
-		Deque<String> livros = new ArryDeque<String>();
+		Deque<String> livros = new ArrayDeque<String>();
 		
 		livros.add("O Peqeuno Principe");
 		livros.push("Don Quixote");
 		livros.push("O Hobbit");
 		
-		System.out.println(livros.peek());
-		System.out.println(livros.element());
+		for(String livro: livros) {
+			System.out.println(livro);
+		}
+		
+		System.out.println(livros.peek()); // return null when it's an empty stack
+		System.out.println(livros.element()); // return a java exception when it's an empty stack
 		
 		System.out.println(livros.pop());
-		System.out.println(livros.poll());
-		System.out.println(livros.remove());
+		System.out.println(livros.poll()); // remove the element and return null when it's an empty stack
+		System.out.println(livros.remove()); //remove the element and return a java exception when it's an empty stack
 		
-		livros.size();
-		livros.clear();
-		livros.isEmpty();
-		
+		//livros.size();
+		//livros.clear();
+		//livros.isEmpty();
+		//livros.contains(livros);
 		
 	}
 
