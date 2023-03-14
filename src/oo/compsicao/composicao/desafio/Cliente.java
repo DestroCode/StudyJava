f42355a5f035f8b29b9f2f396ff9123ed96492cf
DestroCode <aristonlanps@gmail.com>
2023-03-13 21:18:25 -0300

    Challenge OO build1

--- src/oo/compsicao/composicao/desafio/Cliente.java
+++ src/oo/compsicao/composicao/desafio/Cliente.java
@@ -1,5 +1,18 @@
 package oo.compsicao.composicao.desafio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
+	
+	String nome;
+	List<Compra> compras = new ArrayList<Compra>();
+	
+	int valorTotal() {
+//		if(!true)
+//			return 1;
+//		else
+			return 0;
+	}
 
 }
--- src/oo/compsicao/composicao/desafio/Compra.java
+++ src/oo/compsicao/composicao/desafio/Compra.java
@@ -1,5 +1,13 @@
 package oo.compsicao.composicao.desafio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Compra {
+	
+	String id;
+	List<Item> items = new ArrayList<Item>();
+	
+	
 
 }
